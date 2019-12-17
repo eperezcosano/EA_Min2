d443b23796843a30dfa7c2dff7ad64b5e4922765
Izan Pérez Cosano <izanperez98@gmail.com>
2019-12-17 12:23:51 +0100

    subject service

---
+++ EA_Min2/src/app/services/subject.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {Student} from "../models/student";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Subject} from "../models/subject";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+
+  subjectUrl: string = 'http://localhost:3000/subjects';
+
+  constructor(private http: HttpClient) { }
+
+  getSubjects(): Observable<Subject[]> {
+    return this.http.get<Subject[]>(this.subjectUrl);
+  }
+
+  getStudentsFromSubject(subject: string): Observable<Subject> {
+    return this.http.get<Subject>(this.subjectUrl + '/' + subject);
+  }
+
+}
